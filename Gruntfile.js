23d7f3296b1a5ae578bb01e955f71c5a79cc49e7
Gwyn Jones <m@gwnjns.com>
2015-08-24 18:05:02 +0100

    Addition of BrowserSync as an option for GruntJS usage

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,13 +15,28 @@ module.exports = function (grunt) {
                 files: 'css/sass/*.scss',
                 tasks: ['sass']
             }
+        },
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src : [
+                        'css/*.css'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    proxy: 'tna-website-dev:8888'
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browser-sync');
 
     // Default task(s).
-    grunt.registerTask('default', ['sass']);
+    grunt.registerTask('default', ['sass', 'watch']);
+    grunt.registerTask('bSync', ['browserSync', 'watch']);
 
 };
