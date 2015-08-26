66cb38655562f89d216568ebec9dd33fecf76c0d
Gwyn Jones <m@gwnjns.com>
2015-08-26 18:53:04 +0100

    Adding CSS minification to Grunt and changing associated references

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,22 +4,35 @@ module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         sass: {
+            options: {
+                sourcemap: 'none'
+            },
             dist: {
                 files: {
                     'css/base-sass.css': 'css/sass/base-sass.scss'
                 }
             }
         },
+        cssmin: {
+            options: {
+                sourceMap: true
+            },
+            target: {
+                files: {
+                    'css/base-sass.css.min': ['css/base-sass.css']
+                }
+            }
+        },
         watch: {
             css: {
                 files: 'css/sass/*.scss',
-                tasks: ['sass']
+                tasks: ['sass', 'cssmin']
             }
         },
         browserSync: {
             dev: {
                 bsFiles: {
-                    src : [
+                    src: [
                         'css/*.css'
                     ]
                 },
@@ -34,9 +47,10 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-browser-sync');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 
     // Default task(s).
-    grunt.registerTask('default', ['sass', 'watch']);
+    grunt.registerTask('default', ['sass', 'cssmin', 'watch']);
     grunt.registerTask('bSync', ['browserSync', 'watch']);
 
 };
