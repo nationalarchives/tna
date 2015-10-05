5283ce8d56cd9cbb393158e0c91f8e0a9ba5443e
Andy Aldridge <andy@grange85.co.uk>
2015-10-05 16:46:06 +0100

    Update OGL link in footer and remove redundant include files

--- inc/centenary-map/footer.php
+++ inc/centenary-map/footer.php
@@ -3,8 +3,8 @@
     <div class="logo"><a href="http://www.nationalarchives.gov.uk" title="Visit The National Archives' website" target="_blank"><img src="<?php bloginfo('template_url'); ?>/images/centenary-map/tna-centenary-the-national-archives-logo.png" alt="The National Archives logo" /></a>
  <a href="http://www.nationalarchives.gov.uk/first-world-war/" title="Visit The National Archives' First World War 100 website" target="_blank"> <div class="hide-text ww100">First World War 100</div></a>
     </div>
-    <div id="fleft" class="margin-top-large"> <a href="http://www.nationalarchives.gov.uk/doc/open-government-licence/version/2/" target="_blank"><img src="http://www.nationalarchives.gov.uk/images/global/logo-ogl.png" alt="Open Government License logo" border="0" /></a>
-      <p>All content is available under the <a href="http://www.nationalarchives.gov.uk/doc/open-government-licence/version/2" target="_blank">Open Government Licence v2.0</a>, except where otherwise stated</p>
+    <div id="fleft" class="margin-top-large"> <a href="http://www.nationalarchives.gov.uk/doc/open-government-licence/" target="_blank"><img src="http://www.nationalarchives.gov.uk/images/global/logo-ogl.png" alt="Open Government License logo" border="0" /></a>
+      <p>All content is available under the <a href="http://www.nationalarchives.gov.uk/doc/open-government-licence/" target="_blank">Open Government Licence v3.0</a>, except where otherwise stated</p>
     </div>
     <div class="clear20px"></div>
   </div>
