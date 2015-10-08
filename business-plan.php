4777f47961bf9cf3c6af6637e1f5597a0fbced36
Chris Bishop <domingobishop@gmail.com>
2015-10-08 15:48:38 +0100

    quick tidy up

--- business-plan.php
+++ business-plan.php
@@ -33,14 +33,11 @@ get_header(); ?>
 </section>
 <section class="tna-business-plan">
     <div id="page_wrap" class="container" role="main">
-        <!-- Page content -->
         <div class="breather">
             <div class="row">
                 <div class="col starts-at-full ends-at-two-thirds box clr">
                     <h2>Over the next four years we will think and organise ourselves differently, to meet the needs of each of our major audiences and to face our biggest challenge – digital.</h2>
                     <p>We will change the way you think about archives.</p>
-
-                    <!-- closes grid-within-grid -->
                 </div>
                 <div class="col starts-at-full ends-at-one-third box clr bp-download">
                     <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/business-plan/ai-icon.jpg"/><br><small>Read full plan (PDF)</small></a>
@@ -78,28 +75,19 @@ get_header(); ?>
                 <div class="col starts-at-full ends-at-half box clr">
                     <div class="breather">
                     <p class="bp-highlight">We will provide <span>expert advice</span> and scrutiny to government, making sure that the record survives and thrives</p>
-
                     <p>We are both the custodian of the public record and the government’s expert in the management,
                         preservation and use of information. We are uniquely placed by virtue of our history,
                         responsibilities and expertise to provide trusted and independent advice and services across
                         government and the wider public sector. </p>
                     <ul class="stats">
-                        <!-- <li>
-                            <div>Around</div>
-                            <span>5,000</span>
-
-                            <p>pieces of new legislation published in 2014-15</p>
-                        </li> -->
                         <li>
                             <div>Helped move UK to</div>
                             <span>1st place</span>
-
                             <p>in the Global Open Data Index for Legislation</p>
                         </li>
                         <li>
                             <div>More than</div>
                             <span>1 million</span>
-
                             <p>people use the UK Government Web Archive every month</p>
                         </li>
                     </ul>
@@ -118,35 +106,20 @@ get_header(); ?>
                 <div class="col starts-at-full ends-at-half box clr">
                     <div class="breather">
                     <p class="bp-highlight">We will inspire the public with new ways of using and <span>experiencing our collection</span></p>
-
                     <p> We are a window on more than 1,000 years of the nation’s history and our collection at Kew is
                         open to everyone, for free.</p>
                         <p>Many of our users tell us that visiting The National Archives is a life changing experience. We
                         want to give even more people the chance to have that same feeling, whether they set foot on our
                         site at Kew or encounter us online. </p>
                     <ul class="stats">
-                        <!-- <li>
-                            <div>Over</div>
-                            <span>640,000</span>
-
-                            <p>physical records were delivered to visitors in our reading rooms</p>
-                        </li>
-                        <li>
-                            <div>Over</div>
-                            <span>1 million</span>
-
-                            <p>people watched each episode of Portillio's State Secrets </p>
-                        </li> -->
                         <li>
                             <div>Broadcast to over</div>
                             <span>270,000</span>
-
                             <p>students globally with Discovery Education of our Magna Carta assembly</p>
                         </li>
                         <li>
                             <div>Nearly</div>
                             <span>200 million</span>
-
                             <p>downloads of documents occurred last year</p>
                         </li>
                     </ul>
@@ -158,38 +131,27 @@ get_header(); ?>
                             ARCHIVES INSPIRE<br>
                             <img src="<?php echo get_template_directory_uri(); ?>/images/business-plan/curiosity.png" />
                         </div>
-
                     </div>
                 </div>
             </section>
-
             <section class="row" aria-labelledby="archives-sector-tab" role="tabpanel" aria-hidden="false">
                 <div class="col starts-at-full ends-at-half box clr">
                     <div class="breather">
                     <p class="bp-highlight">We will be an <span>effective leader</span> and partner for the archives sector, to sustain and develop the nation’s collection</p>
-
                     <p>The nation’s collection is made up of thousands of archives, each with its own unique history. We
                         lead the wider archive sector in England and perform the Historical Manuscripts Commission's
                         functions in relation to records in private ownership. We sit at the heart of a diverse network
                         of archives and our approach to leading and supporting this network will reflect that diversity.
                     </p>
                     <ul class="stats">
-                        <!-- <li>
-                            <div>In total</div>
-                            <span>32</span>
-
-                            <p>archive services accredited across UK</p>
-                        </li> -->
                         <li>
                             <div>Received</div>
                             <span>£1 million</span>
-
                             <p>funding for Transforming Archives project</p>
                         </li>
                         <li>
                             <div>Discovery contains</div>
                             <span>10 million</span>
-
                             <p>descriptions of records held in 2,500 archives across UK</p>
                         </li>
                     </ul>
@@ -204,34 +166,24 @@ get_header(); ?>
                     </div>
                 </div>
             </section>
-
             <section class="row" aria-labelledby="academic-tab" role="tabpanel" aria-hidden="false">
                 <div class="col starts-at-full ends-at-half box clr">
                     <div class="breather">
                     <p class="bp-highlight">We will advance knowledge through exemplary academic liaison and <span>outstanding interdisciplinary research</span></p>
-
                     <p>We are one of the world’s most valuable resources for academic research. We have strong links
                         with the academic sector and we have set ourselves clear research priorities centred on the
                         challenges we face. Our staff are engaged in collaborative projects on the cutting edge of
                         physical and digital record preservation, historical research and data science. But we believe
                         we can and should do more. </p>
                     <ul class="stats">
-                        <!-- <li>
-                            <div>We've taught</div>
-                            <span>47,000</span>
-
-                            <p>students on site and virtually over past 4 years</p>
-                        </li> -->
                         <li>
                             <div>Over</div>
                             <span>65,000</span>
-
                             <p>records of immigrants in medieval England released</p>
                         </li>
                         <li>
                             <div>We captured</div>
                             <span>290,000</span>
-
                             <p>State Papers images from the 18th century</p>
                         </li>
                     </ul>
@@ -246,12 +198,10 @@ get_header(); ?>
                     </div>
                 </div>
             </section>
-
             <section class="row" aria-labelledby="digital-challenge-tab" role="tabpanel" aria-hidden="false">
                 <div class="col starts-at-full ends-at-half box clr">
                     <div class="breather">
                     <p class="bp-highlight">We will become a <br><span>digital archive by design</span></p>
-
                     <p>Over the last few years we have responded to changes in digital technology ways that have
                         benefitted each of our key audiences.</p>
                         <p>We created Discovery, the first ever comprehensive national online catalogue, a digital records
@@ -261,15 +211,8 @@ get_header(); ?>
                         <li>
                             <div>Website visits up by</div>
                             <span>18%</span>
-
                             <p>year on year</p>
                         </li>
-                        <!-- <li>
-                            <div>By 2020 we'll ingest</div>
-                            <span>2PB data</span>
-
-                            <p>from government digital records</p>
-                        </li> -->
                         <li>
                             <div>Operation War Diary</div>
                             <span>Best of Web</span>
@@ -293,9 +236,6 @@ get_header(); ?>
 </section>
 <section class="tna-business-plan">
     <div class="testimonials-back bp-slider bp-backBlue">
-
-            <!-- Page content -->
-
                 <div class="row">
                     <div class="starts-at-full ends-at-full clr">
                         <ul class="bp-slides">
@@ -370,11 +310,9 @@ get_header(); ?>
                         </ul>
                     </div>
                 </div>
-
-
     </div>
 </section>
-<!-- end page content -->
+
 <?php
 
 get_footer(); ?>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
