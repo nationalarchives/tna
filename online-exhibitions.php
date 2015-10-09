6ef76f5f933d0c29c1483b7d7045521db6eaad0a
Chris Bishop <domingobishop@gmail.com>
2015-10-09 11:07:55 +0100

    copy/paste from dev

--- online-exhibitions.php
+++ online-exhibitions.php
@@ -1,213 +1,204 @@
 <?php
-
+/*
+Template Name: Online Exhibition
+*/
 
 get_header(); ?>
 
-<section class="online-exhibitions">
-    <!-- Featured Image -->
-    <?php // $featureImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );?>
 
-    <div class="row  small">
-        <div class="banner">
-            <div class="container">
-                <div class="row" id="holds-breadcrumb">
-                    <div class="col starts-at-half clr">
-                        <!-- Breadcrumbs -->
-                        <?php include 'breadcrumb.php'; ?>
-                    </div>
-                </div>
-                <div class="col starts-at-full clr">
-                    <div class="transbox">
-                        <h1>Online Exhibitions</h1>
-                    </div>
-
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-<div id="page_wrap" class="container online-exhibitions" role="main">
-    <div class="clr">
-        <div class="filter-bg">
-            <div class="row">
-
-                <div class="col starts-at-full ends-at-half">
-                    <div class="breather clr">
-                        <div class="float-left">
-                            <label class="margin-top-small margin-bottom-small margin-right-large">Sorted by:</label>
-                            <a class="button" href="?sorted=latest"<?php if ($sorted == "latest") ?>>Latest</a>
-                            <a class="button" href="?sorted=a-z-by-title"<?php if ($sorted == "a-z-by-title") ?>>A - Z
-                                by
-                                title</a>
-                            <a class="button"
-                               href="?sorted=historical-period" <?php if ($sorted == "historical-period") ?>>Historical</a>
-                        </div>
-                        <!-- end float-left -->
-                    </div>
-                </div>
-
-                <div class="col starts-at-full ends-at-half">
-                    <div class="breather clr float-right">
-                        <label class="margin-top-small margin-bottom-small margin-right-large">View page:</label>
+<?php
 
 
-                    </div>
-                    <!-- end float-right -->
-                </div>
-            </div>
-            <!--end breather -->
-        </div>
-        <!--end select-float -->
-    </div>
-    <div id="index" class="row">
-        <?php
-        /*
-        $sorted = $_GET['sorted'];
-        if ($sorted == "latest") {
-            $order = "orderby=date&order=DESC";
-        }
-        if ($sorted == "a-z-by-title") {
-            $order = "orderby=title&order=ASC";
-        }
-        if ($sorted == "historical-period") {
-            $order = "orderby=menu_order&order=DESC";
-        }
+if ( have_posts() ) : ?>
+<?php /* Start the Loop */ ?>
+<?php while ( have_posts() ) : the_post();
 
-        $onlineExhibitions = new WP_Query('post_type=online-exhibitions&' . $order);
-        $count = 0;
-        if ($onlineExhibitions->have_posts()) :
-            while ($onlineExhibitions->have_posts()) :
-                $onlineExhibitions->the_post()
-        */
+        $image_id = get_post_thumbnail_id($page->ID);
+        $image_url = wp_get_attachment_image_src($image_id,'full', false);
+
+
+
+
         ?>
-        <div
-            class="col starts-at-full ends-at-half<?php // $count <= 1 ? 'ends-at-half' : 'ends-at-one-third' ?> clr box">
-            <div class="breather">
-                <a href="#" title="title">
-                    <div class="float-right starts-at-full ends-at-half thumbnail-container-lrg">
-                        <img src="wp-content/themes/tna/images/backgrounds/bg-online-exhibitions.jpg"/></div>
-                    <div class="overlay width-full">
-                        <h2>1901: Living at the time of the census</h2>
 
-                        <p>Find out what life was like in 1901 and learn a about the census and how to discover more
-                            about the people and places in it.</p>
-                    </div>
-                </a>
-            </div>
-        </div>
-        <div
-            class="col starts-at-full ends-at-half<?php // $count <= 1 ? 'ends-at-half' : 'ends-at-one-third' ?> clr box">
-            <div class="breather">
-                <a href="#" title="title">
-                    <div class="float-right starts-at-full ends-at-half thumbnail-container-lrg">
-                        <img src="wp-content/themes/tna/images/backgrounds/bg-online-exhibitions.jpg"/></div>
-                    <div class="overlay width-full">
-                        <h2>1901: Living at the time of the census</h2>
+<div id="page_wrap" class="container" role="main" >
+  
+  <!-- Breadcrumbs // Image descriptor -->
+  
+  <div class="row" id="exhibition-breadcrumbs">
+    <?php include 'breadcrumb.php'; ?>
 
-                        <p>Find out what life was like in 1901 and learn a about the census and how to discover more
-                            about the people and places in it.</p>
-                    </div>
-                </a>
-            </div>
-        </div>
+    <div class="sprite icon-img-desc position-top-right">
+      <div class="image-description" style="display: block;">
+        <?php
+        //1.Banner-information custom field
+
+        $banner_info =  get_post_meta(get_the_ID(), '1.Banner-information', TRUE);
+
+        if ($banner_info){
+
+            echo ($banner_info);
+
+
+        }
+
+        ?>
+      </div>
     </div>
-    <div class="row">
-        <div
-            class="col starts-at-full ends-at-one-third<?php // $count <= 1 ? 'ends-at-half' : 'ends-at-one-third' ?> clr box">
-            <div class="breather">
-                <a href="#" title="title">
-                    <div class="float-right starts-at-full ends-at-half thumbnail-container-lrg">
-                        <img src="wp-content/themes/tna/images/backgrounds/bg-online-exhibitions.jpg"/></div>
-                    <div class="overlay width-full">
-                        <h2>1901: Living at the time of the census</h2>
 
-                        <p>Find out what life was like in 1901 and learn a about the census and how to discover more
-                            about the people and places in it.</p>
-                    </div>
-                </a>
-            </div>
-        </div>
-        <div
-            class="col starts-at-full ends-at-one-third<?php // $count <= 1 ? 'ends-at-half' : 'ends-at-one-third' ?> clr box">
-            <div class="breather">
-                <a href="#" title="title">
-                    <div class="float-right starts-at-full ends-at-half thumbnail-container-lrg">
-                        <img src="wp-content/themes/tna/images/backgrounds/bg-online-exhibitions.jpg"/></div>
-                    <div class="overlay width-full">
-                        <h2>Help with your research</h2>
 
-                        <p>Find out what life was like in 1901 and learn a about the census and how to discover more
-                            about the people and places in it.</p>
-                    </div>
-                </a>
-            </div>
-        </div>
-        <div
-            class="col starts-at-full ends-at-one-third<?php // $count <= 1 ? 'ends-at-half' : 'ends-at-one-third' ?> clr box">
-            <div class="breather">
-                <a href="#" title="title">
-                    <div class="float-right starts-at-full ends-at-half thumbnail-container-lrg">
-                        <img src="wp-content/themes/tna/images/backgrounds/bg-online-exhibitions.jpg"/></div>
-                    <div class="overlay width-full">
-                        <h2>Help with your research</h2>
+  </div>
 
-                        <p>Find out what life was like in 1901 and learn a about the census and how to discover more
-                            about the people and places in it.</p>
-                    </div>
-                </a>
-            </div>
-        </div>
-        <div
-            class="col starts-at-full ends-at-one-third<?php // $count <= 1 ? 'ends-at-half' : 'ends-at-one-third' ?> clr box">
-            <div class="breather">
-                <a href="#" title="title">
-                    <div class="float-right starts-at-full ends-at-half thumbnail-container-lrg">
-                        <img src="wp-content/themes/tna/images/backgrounds/bg-online-exhibitions.jpg"/></div>
-                    <div class="overlay width-full">
-                        <h2>Help with your research</h2>
 
-                        <p>Find out what life was like in 1901 and learn a about the census and how to discover more
-                            about the people and places in it.</p>
-                    </div>
-                </a>
-            </div>
-        </div>
-        <div
-            class="col starts-at-full ends-at-one-third<?php // $count <= 1 ? 'ends-at-half' : 'ends-at-one-third' ?> clr box">
-            <div class="breather">
-                <a href="#" title="title">
-                    <div class="float-right starts-at-full ends-at-half thumbnail-container-lrg">
-                        <img src="wp-content/themes/tna/images/backgrounds/bg-online-exhibitions.jpg"/></div>
-                    <div class="overlay width-full">
-                        <h2>Help with your research</h2>
+<!-- All content goes here -->
 
-                        <p>Find out what life was like in 1901 and learn a about the census and how to discover more
-                            about the people and places in it.</p>
-                    </div>
-                </a>
-            </div>
+  <div class="row exhibition-top">
+  <div class="col starts-at-full ends-at-full">
+   <div class="breather">
+          <h1>
+            <?php the_title();?></h1>
         </div>
-        <div
-            class="col starts-at-full ends-at-one-third<?php // $count <= 1 ? 'ends-at-half' : 'ends-at-one-third' ?> clr box">
-            <div class="breather">
-                <a href="#" title="title">
-                    <div class="float-right starts-at-full ends-at-half thumbnail-container-lrg">
-                        <img src="wp-content/themes/tna/images/backgrounds/bg-online-exhibitions.jpg"/></div>
-                    <div class="overlay width-full">
-                        <h2>Help with your research</h2>
+    
+   
+  </div>
 
-                        <p>Find out what life was like in 1901 and learn a about the census and how to discover more
-                            about the people and places in it.</p>
-                    </div>
-                </a>
-            </div>
-        </div>
-    </div>
-    <?php
-    // $count++;
-    // endwhile; endif;
-    ?>
+  <div class="col starts-at-full ends-at-full white-bg">
+
+    <div class="col starts-at-full ends-at-full introduction">
+    <div class="breather">
+<?php the_content();?>
+</div></div>
+
+<!--   <div class="col starts-at-full ends-at-half">-->
+<!--    <div class="breather">-->
+<!---->
+<!--Additional content?-->
+<!---->
+<!--</div></div>-->
+
+
+  </div>
+
+
+
+  <div class="col starts-at-full ends-at-full">
+
+  
+    <div id="documents">
+
+<!-- document tiles in a masonry style -->
+<div id="document-container">
+
+  <?php
+        $pages = get_pages('sort_column=menu_order&child_of='.$post->ID.'&parent='.$post->ID.'');
+
+        foreach($pages as $page)
+        {
+            $title = $page->post_title;
+
+            ?>
+
+
+<!-- documents in loop -->
+
+
+
+<div class="dark-grey-border box-small document-box">
+  <div class="secret-box">
+
+
+      <div class="secret-title"><h2><a href="<?php echo get_page_link($page->ID); ?>" title="<?php echo ($title);?>"><?php echo ($title);?></a></h2>
+          <p>  <?php if ( empty( $page->post_excerpt ) ) {
+                  echo first_sentence($page->post_content);
+              } else {
+                  echo $page->post_excerpt;
+              }
+
+
+              ?>
+          </p></div>
+
+      <?php
+
+      $thumb_id = get_post_thumbnail_id($page->ID);
+      $thumb_url = wp_get_attachment_image_src($thumb_id,'medium', false);
+
+      if ($thumb_url){
+
+          ?>
+
+
+      <a href="<?php echo get_page_link($page->ID); ?>" title="<?php echo ($title);?>"><img src="<?php echo($thumb_url[0]); ?>" alt="<?php echo ($title);?>"></a>
+
+<?php }?>
+</div>
+
+</div>
+
+<!-- documents in loop -->
+
+        <?php
+        }
+
+        ?>
+
+<!-- container ends-->
+<div class="clear-both"></div>
 </div>
 </div>
-<!-- end page content -->
+</div>
+<!-- document tiles in a masonry style -->
+
+
+
+
+  </div>
+
+
+
+ 
+  <!-- end col --> 
+</div>
+
+
+
+
+<!-- All content goes here -->  
+</div>
+
+
+
+
+
+
+
+
+<?php endwhile; ?>
+<?php endif; ?>
+
+<!-- Custom stylesheets -->
+
+
+<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/css/online-exhibition.css?=timestamp=<?php echo(time());?>">
+<?php    if ($image_url){?>
+    <style>
+
+        body{
+            background-image: url(<?php echo($image_url[0]); ?>);
+            background-repeat: no-repeat;
+
+        }
+
+    </style>
+<?php }?>
+
+
+<!--[if lte IE 9]>
+<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/css/online-exhibition-ie.css">
+<![endif]-->
+
+
 <?php get_footer(); ?>
+
+
+
