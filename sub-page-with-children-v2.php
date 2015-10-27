1fbc2fd835e303d4e071677db3345942a8c0864d
Punal Chotrani <punalsc@gmail.com>
2015-10-27 16:58:15 +0000

    Added added new version for the template

---
+++ sub-page-with-children-v2.php
@@ -0,0 +1,204 @@
+<?php
+/*
+Template Name: Sub-section page with children Version 2
+*/
+
+get_header(); ?>
+<?php if ( have_posts() ) : ?>
+<?php /* Start the Loop */ ?>
+<?php while ( have_posts() ) : the_post(); ?>
+
+<div id="page_wrap" class="container" role="main">
+
+  <!-- Breadcrumbs -->
+  <?php include 'breadcrumb.php'; ?>
+
+  <!-- Page content -->
+  <div class="row">
+    <div class="col starts-at-full ends-at-full box clr">
+      <div class="heading-holding-banner">
+        <h1><span><span>
+          Selecting and transferring records          </span> </span> </h1>
+      </div>
+      <div class="breather">
+        <div class="col starts-at-full ends-at-two-thirds clr">
+          <p>These pages provide guidance on the selection and transfer of records. Bodies transferring records to The National Archives or to places of deposit under the Public Records Act 1958 should follow this process.</p>
+<p>This process applies to records in all formats and media, including paper and digital records.</p>
+        </div>
+
+
+
+        <div class="col starts-at-full ends-at-one-third clr feature-box">
+          <div class="breather">
+            <p>Check whether your organisation needs to transfer records either to The National Archives or to a place of deposit by reading <a title="PDF, opens in a new window" href="/documents/information-management/how-to-identify-a-public-records-body.pdf" target="_self">Public records bodies: Determination and change of status</a> (PDF, 0.22MB)</p>
+<p>Alternatively <a href="mailto:information.management@nationalarchives.gsi.gov.uk">contact us</a> for more information.</p>
+          </div>
+        </div>
+
+              </div>
+    </div>
+  </div>
+  <div class="row" id="index">
+        <div class="col starts-at-full ends-at-half clr box">
+      <div class="heading-holding-banner">
+        <h2><span><span><a href="http://www.nationalarchives.gov.uk/information-management/manage-information/selection-and-transfer/appraising-records/">Step 1: Appraising your records</a></span></span></h2>
+      </div>
+      <div class="breather">
+
+                        <a href="http://www.nationalarchives.gov.uk/information-management/manage-information/selection-and-transfer/appraising-records/" title="Step 1: Appraising your records"> <div class="float-right starts-at-full ends-at-half thumbnail-container-lrg" style="background-image: url(http://livelb.nationalarchives.gov.uk/wp-content/uploads/2014/02/im-st-records-01-appraisal1.jpg); background-repeat: no-repeat">
+                  </div></a>
+
+
+          <p>
+          Records can possess different types or degrees of value to a public records body, and these values affect how records are managed and how long they need to be kept.        </p>
+        <ul class="child">
+          <li class="page_item page-item-1561"><a href="http://www.nationalarchives.gov.uk/information-management/manage-information/selection-and-transfer/appraising-records/schedules-for-keeping-disposing-of-records/">Schedules for keeping and disposing</a></li>
+
+        </ul>
+      </div>
+    </div>
+        <div class="col starts-at-full ends-at-half clr box">
+      <div class="heading-holding-banner">
+        <h2><span><span><a href="http://www.nationalarchives.gov.uk/information-management/manage-information/selection-and-transfer/selecting-records/">Step 2: Selecting your records</a></span></span></h2>
+      </div>
+      <div class="breather">
+
+                        <a href="http://www.nationalarchives.gov.uk/information-management/manage-information/selection-and-transfer/selecting-records/" title="Step 2: Selecting your records"> <div class="float-right starts-at-full ends-at-half thumbnail-container-lrg" style="background-image: url(http://livelb.nationalarchives.gov.uk/wp-content/uploads/2014/02/im-st-records-02-selection1.jpg); background-repeat: no-repeat">
+                  </div></a>
+
+
+          <p>
+          Under the Public Records Act 1958, those responsible for public records must make arrangements for the selection of those records which ought to be permanently preserved.        </p>
+        <ul class="child">
+          <li class="page_item page-item-1636"><a href="http://www.nationalarchives.gov.uk/information-management/manage-information/selection-and-transfer/selecting-records/osp-subject/">Operational selection policies by subject</a></li>
+<li class="page_item page-item-1579"><a href="http://www.nationalarchives.gov.uk/information-management/manage-information/selection-and-transfer/selecting-records/osp-number/">Operational selection policies by number</a></li>
+
+        </ul>
+      </div>
+    </div>
+        <div class="col starts-at-full ends-at-half clr box">
+      <div class="heading-holding-banner">
+        <h2><span><span><a href="http://www.nationalarchives.gov.uk/information-management/manage-information/selection-and-transfer/sensitivity-reviews-on-selected-records/">Step 3: Sensitivity reviews of selected records</a></span></span></h2>
+      </div>
+      <div class="breather">
+
+                        <a href="http://www.nationalarchives.gov.uk/information-management/manage-information/selection-and-transfer/sensitivity-reviews-on-selected-records/" title="Step 3: Sensitivity reviews of selected records"> <div class="float-right starts-at-full ends-at-half thumbnail-container-lrg" style="background-image: url(http://livelb.nationalarchives.gov.uk/wp-content/uploads/2014/02/im-st-records-03-sensitivity1.jpg); background-repeat: no-repeat">
+                  </div></a>
+
+
+          <p>
+          Departments should have effective arrangements to determine which records should be retained in the department, designated as open on transfer or transferred as closed for a period of time.        </p>
+        <ul class="child">
+
+        </ul>
+      </div>
+    </div>
+        <div class="col starts-at-full ends-at-half clr box">
+      <div class="heading-holding-banner">
+        <h2><span><span><a href="http://www.nationalarchives.gov.uk/information-management/manage-information/selection-and-transfer/cataloguing-and-preparation-of-records/">Step 4: Cataloguing and preparation of records</a></span></span></h2>
+      </div>
+      <div class="breather">
+
+                        <a href="http://www.nationalarchives.gov.uk/information-management/manage-information/selection-and-transfer/cataloguing-and-preparation-of-records/" title="Step 4: Cataloguing and preparation of records"> <div class="float-right starts-at-full ends-at-half thumbnail-container-lrg" style="background-image: url(http://livelb.nationalarchives.gov.uk/wp-content/uploads/2014/02/im-st-records-04-preparation1.jpg); background-repeat: no-repeat">
+                  </div></a>
+
+
+          <p>
+          This is the process of preparing records for permanent preservation.        </p>
+        <ul class="child">
+          <li class="page_item page-item-1602"><a href="http://www.nationalarchives.gov.uk/information-management/manage-information/selection-and-transfer/cataloguing-and-preparation-of-records/physically-preparing-records/">Physically preparing your records</a></li>
+<li class="page_item page-item-1604"><a href="http://www.nationalarchives.gov.uk/information-management/manage-information/selection-and-transfer/cataloguing-and-preparation-of-records/cataloguing-records/">Cataloguing your records</a></li>
+
+        </ul>
+      </div>
+    </div>
+        <div class="col starts-at-full ends-at-half clr box">
+      <div class="heading-holding-banner">
+        <h2><span><span><a href="http://www.nationalarchives.gov.uk/information-management/manage-information/selection-and-transfer/delivery-of-records/">Step 5: Planning and arranging delivery of records</a></span></span></h2>
+      </div>
+      <div class="breather">
+
+                        <a href="http://www.nationalarchives.gov.uk/information-management/manage-information/selection-and-transfer/delivery-of-records/" title="Step 5: Planning and arranging delivery of records"> <div class="float-right starts-at-full ends-at-half thumbnail-container-lrg" style="background-image: url(http://livelb.nationalarchives.gov.uk/wp-content/uploads/2014/02/im-st-records-05-uplift1.jpg); background-repeat: no-repeat">
+                  </div></a>
+
+
+          <p>
+          The process of transporting the records from the department to The National Archives is also known as 'uplift'.        </p>
+        <ul class="child">
+
+        </ul>
+      </div>
+    </div>
+        <div class="col starts-at-full ends-at-half clr box">
+      <div class="heading-holding-banner">
+        <h2><span><span><a href="http://www.nationalarchives.gov.uk/information-management/manage-information/selection-and-transfer/how-we-accession-your-records/">Step 6: Accessioning your records</a></span></span></h2>
+      </div>
+      <div class="breather">
+
+                        <a href="http://www.nationalarchives.gov.uk/information-management/manage-information/selection-and-transfer/how-we-accession-your-records/" title="Step 6: Accessioning your records"> <div class="float-right starts-at-full ends-at-half thumbnail-container-lrg" style="background-image: url(http://livelb.nationalarchives.gov.uk/wp-content/uploads/2014/02/im-st-records-06-accessioning1.jpg); background-repeat: no-repeat">
+                  </div></a>
+
+
+          <p>
+          Accessioning is the work that The National Archives does once records have been delivered, so that we can make the records available.        </p>
+        <ul class="child">
+
+        </ul>
+      </div>
+    </div>
+        <div class="col starts-at-full ends-at-half clr box">
+      <div class="heading-holding-banner">
+        <h2><span><span><a href="http://www.nationalarchives.gov.uk/information-management/manage-information/selection-and-transfer/borrowing-accessioned-records/">Borrowing accessioned records</a></span></span></h2>
+      </div>
+      <div class="breather">
+
+
+
+          <p>
+          The forms and guidance provided below are for government departments that want to borrow accessioned records from The National Archives.        </p>
+        <ul class="child">
+
+        </ul>
+      </div>
+    </div>
+        <div class="col starts-at-full ends-at-half clr box">
+      <div class="heading-holding-banner">
+        <h2><span><span><a href="http://www.nationalarchives.gov.uk/information-management/manage-information/selection-and-transfer/legal-obligations-transfer/">Legal obligations for transfer</a></span></span></h2>
+      </div>
+      <div class="breather">
+
+
+
+          <p>
+          The laws of England and Wales place obligations upon departments and The National Archives for the collection, disposal and preservation of records.         </p>
+        <ul class="child">
+
+        </ul>
+      </div>
+    </div>
+        <div class="col starts-at-full ends-at-half clr box">
+      <div class="heading-holding-banner">
+        <h2><span><span><a href="http://www.nationalarchives.gov.uk/information-management/manage-information/selection-and-transfer/digital-records-transfer/">Digital records transfer</a></span></span></h2>
+      </div>
+      <div class="breather">
+
+
+
+          <p>
+          If you wish to transfer digital records to The National Archives, please get in touch with your Information Management Consultant (IMC) or email our Information Management Team.        </p>
+        <ul class="child">
+          <li class="page_item page-item-2486"><a href="http://www.nationalarchives.gov.uk/information-management/manage-information/selection-and-transfer/digital-records-transfer/digital-transfer-steps/">Digital transfer steps</a></li>
+<li class="page_item page-item-1606"><a href="http://www.nationalarchives.gov.uk/information-management/manage-information/selection-and-transfer/digital-records-transfer/guidance-forms-digital-transfer/">Guidance and forms for digital transfer</a></li>
+<li class="page_item page-item-22483"><a href="http://www.nationalarchives.gov.uk/information-management/manage-information/selection-and-transfer/digital-records-transfer/file-formats-transfer/">File formats for transfer</a></li>
+
+        </ul>
+      </div>
+    </div>
+      </div>
+</div>
+</div>
+<!-- end page content -->
+
+<?php get_footer(); ?>
+
+
