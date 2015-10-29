f7dfb6bc8f6fe60aae7ad78325b9e984086a505b
Punal Chotrani <punalsc@gmail.com>
2015-10-29 10:51:23 +0000

    Removed commented block.

--- sub-page-with-children.php
+++ sub-page-with-children.php
@@ -110,34 +110,3 @@ get_header(); ?>
 <?php endwhile; ?>
 <?php endif; ?>
 <?php get_footer(); ?>
-
-//To be continued
-<?php
-
-/*$args = array(
-    'post_type'      => 'page',
-    'posts_per_page' => -1,
-    'post_parent'    => $post->ID,
-    'order'          => 'ASC',
-    'orderby'        => 'menu_order'
-);
-
-
-$parent = new WP_Query( $args );
-
-if ( $parent->have_posts() ) :*/ ?>
-
-    <?php //while ( $parent->have_posts() ) : $parent->the_post(); ?>
-
-        <!--<div id="parent-<?php /*the_ID(); */?>" class="parent-page">
-
-            <h1><a href="<?php /*the_permalink(); */?>" title="<?php /*the_title(); */?>"><?php /*the_title(); */?></a></h1>
-
-            <p><?php /*the_advanced_excerpt(); */?></p>
-
-        </div>-->
-
-    <?php //endwhile; ?>
-
-<?php// endif; wp_reset_query();
-?>
