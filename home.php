54fcca0b21e79eeb889bd6cf959e87af199b327c
SDWilkes <simonwilkes@gmail.com>
2015-11-09 14:38:35 +0000

    Adding include with promotion area to homepage, added GA code to button

--- home.php
+++ home.php
@@ -23,14 +23,27 @@ get_header(); ?>
       </div>
       </article>
     </div>
+
+<?php
+
+    include 'inc/home-promotion.php';
+
+?>
+
+    <!--
+
+    // Newsletter sign-up
+
   <div class="row">
     <div class="col starts-at-full ends-at-full box banner-newsletter">
             <span class="float-left pad-medium">
           <h2 class="margin-none pad-none">Sign up to our enewsletter for regular news and offers</h2>
         </span>
         <form name="signup" id="banner-form" class="pad-medium" action="http://dmtrk.co.uk/signup.ashx" method="post"><input type="hidden" name="addressbookid" value="281378"><input type="hidden" name="userid" value="28895"><input type="hidden" name="ReturnURL" value="http://nationalarchives.gov.uk/news/subscribe-confirmation.htm"><input type="email" id="Email" name="Email" required placeholder="Enter your email address"><input id="newsletterSignUp" type="submit" value="Sign up" class="button"></form>
-    </div><!-- end col -->
-  </div><!-- end row -->
+    </div>
+  </div>
+
+  -->
 
 
 <div class="row">
