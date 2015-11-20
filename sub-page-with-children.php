307c8e28af9b7e7eb40fafd3a3e1e93d91f52a3f
Punal Chotrani <punalsc@gmail.com>
2015-11-20 11:32:59 +0000

    Added if (has_excerpt ();

--- sub-page-with-children.php
+++ sub-page-with-children.php
@@ -84,10 +84,10 @@ get_header(); ?>
            ?>
            <p>
              <?php //echo first_sentence(get_the_content());
-                 if ( empty( $page->post_excerpt ) ) {
-                     echo first_sentence(get_the_content());
+                 if ( has_excerpt( $post->ID ) ) {
+                     echo the_excerpt();
                  } else {
-                     echo $page->post_excerpt;
+                     echo first_sentence(get_the_content());
                  }
              ?>
            </p>
