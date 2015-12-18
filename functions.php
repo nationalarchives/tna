67ab74daa531b4acf005bef1e9ebe7b2d1cd8178
Mihai Diaconita <michaiel.webdesigner@gmail.com>
2015-12-18 12:21:13 +0000

    I have removed the emoji script from the header by adding some wp hooks inside functions.php

--- functions.php
+++ functions.php
@@ -841,5 +841,10 @@ function first_paragraph($content){
 add_filter('the_content', 'first_paragraph');
 /* adds a class style to the first paragraph*/
 
+/* Disable the emoji script */
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
 
 ?>
