853fb86bebffecd783f85d1f70f531fc5f088ce5
Andy Aldridge <andy@grange85.co.uk>
2016-01-26 12:13:19 +0000

    update breadcrumb.php to manage child sites

--- breadcrumb.php
+++ breadcrumb.php
@@ -1,19 +1,24 @@
 <div class="row" id="breadcrumb-holder">
     <div class="col starts-at-full clr">
         <div id="breadcrumb">
-            <span class="first"><a href="/">Home</a> &gt;</span>
+            <span class="first"><a href="/">Home</a>&nbsp;&gt;&nbsp;</span>
+            <?php
+            	if ($GLOBALS['tnatheme']['ischildsite']) {
+	            	if (!empty($GLOBALS['tnatheme']['subsitepatharr'])) {
+						foreach ($GLOBALS['tnatheme']['subsitepatharr'] as $key => $value){
+							echo '<a href="' . $value . '">' . $key . '</a>&nbsp;&gt;&nbsp;';
+						}
+	            	}
+	            	if (is_front_page()) {
+	            		echo $GLOBALS['tnatheme']['childsitename'];
+	            	}
+	            }
+            ?>
+
             <?php if ( function_exists('yoast_breadcrumb') ) {
-                yoast_breadcrumb();
+                $breadcrumbs = yoast_breadcrumb('','',false);
+                echo make_urls_root_relative($breadcrumbs);
             } ?>
-
-            <?php if (current_user_can( 'manage_options' )) {
-
-                if (!has_post_thumbnail() ) {
-
-                    ?>
-
-                    <!--<div class="float-right"><a href="/wp-admin/media-upload.php?post_id=<?php //echo $post->ID; ?>&type=image&TB_iframe=1" target="_blank" class="button">Add featured image</a></div>-->
-                <?php } }?>
         </div>
     </div>
 </div>
