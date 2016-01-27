124931d29e8c3bb1ae1e8d1d99ce001ce9ea819c
Andy Aldridge <andy@grange85.co.uk>
2016-01-27 16:42:20 +0000

    remove _blank

--- inc/online-exhibitions-template-part.php
+++ inc/online-exhibitions-template-part.php
@@ -1,7 +1,7 @@
 <div class="breather">
     <?php $exhibition_field = get_post_meta($post->ID, 'online-exhibition', true) ?>
     <?php $thumb = wp_get_attachment_url(get_post_thumbnail_id( $post->ID ), 'medium' ); ?>
-    <a href="<?php echo $exhibition_field ?>" target="_blank" title="<?php the_title(); ?>">
+    <a href="<?php echo $exhibition_field ?>" title="<?php the_title(); ?>">
         <?php if (has_post_thumbnail()) : ?>
             <div class="has-background" style="background: url(<?php echo $thumb ?> ) !important; background-size:cover !important; background-repeat:no-repeat;"></div>
         <?php else: ?>
