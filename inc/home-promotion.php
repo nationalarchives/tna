72fd51de93059e10e063f6ccf874cf5b68b9ae09
SDWilkes <simonwilkes@gmail.com>
2016-01-29 15:32:40 +0000

    Adding banner code, images and inline styles.

--- inc/home-promotion.php
+++ inc/home-promotion.php
@@ -1,16 +1,82 @@
-<div class="row">
-    <div class="col starts-at-full ends-at-full box " style="background-image:url('<?php bloginfo('stylesheet_directory');?>/images/home/tna-eya-bg.gif'); background-repeat: repeat-x; background-position: bottom; border: 1px solid #ccc;">
 
-        <div class="float-left starts-at-full ends-at-one-third margin-none margin-bottom-small">
-            <div class="pad-medium">
-                <img src="<?php bloginfo('stylesheet_directory');?>/images/home/tna-eya-archive-explored.png" alt="Archive Explored" width="100%" style="max-width:280px;"></div>
-        </div>
+<div class="row">
+
+    <div class="col starts-at-full ends-at-full box ">
+        <a href="http://www.bymewilliamshakespeare.org" target="_blank" title=""> <div class="bmws-banner ">
+
+
+        </div></a>
         <div class="float-left starts-at-full ends-at-two-thirds margin-none margin-bottom-small">
-            <div class="pad-medium"><a href="http://exploreyourarchive.org/?utm_source=TNA&utm_medium=Banner&utm_campaign=Explore-your-archive" target="_blank" class="button float-right margin-top-medium" style="background-color:#762367;color:#fff">Find events near you</a><h2 class="margin-none">Explore Your Archive week</h2>
-                14 - 22 November
+
+
+
+            <div class="pad-medium">
+                <?php $key="shakespeare";
+
+                if ($key) {
+                    echo get_post_meta($post->ID, $key, true);
+
+                }?>
+        </div>   </div>
+        <div class="float-left starts-at-full ends-at-one-third margin-none margin-bottom-small">
+            <div class="pad-medium"><a href="http://www.bymewilliamshakespeare.org" target="_blank" class=" button float-right margin-top-medium" style="background-color:#000;color:#fff">Find out more</a>
             </div>
 
 
         </div>
     </div>
 </div>
+
+    <style>
+        .bmws-banner{
+            width:100%;
+            min-height:250px;
+            height:auto;
+            background-image: url("/wp-content/themes/tna/images/backgrounds/tna-bmws-banner-desktop.jpg");
+            background-repeat: no-repeat;
+            background-size: 100% auto;
+        }
+
+        @media only screen and (min-width:320px) {
+
+            .bmws-banner{
+                min-height: 200px;
+                height:auto;
+                background-image: url("/wp-content/themes/tna/images/backgrounds/tna-bmws-banner-mobile.jpg");
+
+            }
+        }
+
+        @media only screen and (min-width: 480px) {
+
+            .bmws-banner{
+                min-height: 145px;
+            height:auto;
+
+                background-image: url("/wp-content/themes/tna/images/backgrounds/tna-bmws-banner-tablet.jpg");
+
+            }
+                            }
+
+
+        @media only screen and (min-width: 768px) {
+
+            .bmws-banner{
+
+                background-image: url("/wp-content/themes/tna/images/backgrounds/tna-bmws-banner-desktop.jpg");
+
+                min-height:125px;
+            }
+        }
+
+        @media only screen and (min-width: 1024px) {
+
+            .bmws-banner{
+
+                background-image: url("/wp-content/themes/tna/images/backgrounds/tna-bmws-banner-desktop.jpg");
+
+                min-height:175px;
+            }
+        }
+
+    </style>
