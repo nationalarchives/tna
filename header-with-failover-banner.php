24b95c7ed6d2eaaacce94560edcb04619b622d87
grange85 <andy@grange85.co.uk>
2016-10-10 12:32:35 +0100

    Updated headers with new search form action

--- header-with-failover-banner.php
+++ header-with-failover-banner.php
@@ -154,14 +154,14 @@ if ($post_description == ""){
     <a href="http://www.nationalarchives.gov.uk" title="Go to The National Archives homepage"> <img src="<?php bloginfo('template_url'); ?>/images/global/logo-white.png" alt="The National Archives" id="logo"/> </a>
     </article>
       <article class="col starts-at-full ends-at-half clr">
-        <form action="http://www.nationalarchives.gov.uk/search/quick_search.aspx" method="get" id="mobileGlobalSearch">
+        <form action="/search/results" method="get" id="mobileGlobalSearch">
           <div id="mobile-search-outer-wrapper">
             <div id="mobile-search-wrapper">
-              <input type="text" id="mobile-search-field" placeholder="Search our website..." name="search_text" required aria-required="true" >
+              <input type="text" id="mobile-search-field" placeholder="Search our website..." name="_q" required aria-required="true" >
               <input type="submit" value="">
             </div>
             <div id="changeDestination">
-              <input checked type="radio" class="mobileSearchDestinationOption" name="searchDestination" id="websiteSearch" data-target="http://www.nationalarchives.gov.uk/search/quick_search.aspx" data-placeholder="Search our website..." data-fieldName="search_text"/>
+              <input checked type="radio" class="mobileSearchDestinationOption" name="searchDestination" id="websiteSearch" data-target="/search/results" data-placeholder="Search our website..." data-fieldName="_q"/>
               <label for="websiteSearch">Search our website</label>
               <input type="radio" class="mobileSearchDestinationOption" name="searchDestination" id="catalogueSearch" data-target="http://discovery.nationalarchives.gov.uk/SearchUI/s/res" data-placeholder="Search our records..." data-fieldName="_q" />
               <label for="catalogueSearch">Search our records</label>
@@ -170,13 +170,13 @@ if ($post_description == ""){
         </form>
 
         <div id="search-field-wrapper">
-          <form action="http://www.nationalarchives.gov.uk/search/quick_search.aspx" method="get" id="globalSearch">
+          <form action="/search/results" method="get" id="globalSearch">
             <span id="scope-selector">&nbsp;</span>
-            <input type="text" class="search-field" placeholder="Search our website..." id="tnaSearch" name="search_text" required aria-required="true" >
+            <input type="text" class="search-field" placeholder="Search our website..." id="tnaSearch" name="_q" required aria-required="true" >
             <input type="submit" class="search-button" id="search-button" value="">
           </form>
           <ul id="search-options">
-            <li><a href="#" class="formDestinationChanger" data-target="http://www.nationalarchives.gov.uk/search/quick_search.aspx" data-placeholder="Search our website..." data-fieldName="search_text" role="button" aria-label="Change form destination to search the website">Search our website</a></li>
+            <li><a href="#" class="formDestinationChanger" data-target="/search/results" data-placeholder="Search our website..." data-fieldName="_q" role="button" aria-label="Change form destination to search the website">Search our website</a></li>
             <li><a href="#" class="formDestinationChanger" data-target="http://discovery.nationalarchives.gov.uk/SearchUI/s/res" data-placeholder="Search our records..." data-fieldName="_q" role="button" aria-label="Change form destination to search the catalogue">Search our records</a></li>
           </ul>
         </div>
