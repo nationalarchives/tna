8dc0f0c00ae7dac1fb5a52639f2a6e1538f397b1
grange85 <andy@grange85.co.uk>
2017-03-17 14:17:29 +0000

    remove line breaks from form to resolve layout issue

--- inc/guided-search-forms/naturalised_britons_1.php
+++ inc/guided-search-forms/naturalised_britons_1.php
@@ -5,16 +5,4 @@
 				<label for="Name">Name</label>
 				<input id="Name" class="width-half" name="_q" />
 			</div>
-		</fieldset>
-		<div class="button-holder">
-			<input alt="Search" class="submit" type="submit" value="Search">
-		</div>
-		<input type="hidden" name="discoveryCustomSearch" value="true" />
-		<input id="Hidden1" name="_ser" type="hidden" value="HO 334" />
-		<input id="Hidden2" name="_ser" type="hidden" value="HO 409" />
-		<input id="Hidden4" name="_ser" type="hidden" value="HO 45" />
-		<input id="Hidden5" name="_ser" type="hidden" value="HO 144" />
-		<input id="Hidden6" name="_ser" type="hidden" value="HO 382" />
-		<input id="Hidden7" name="_hb" type="hidden" value="tna" />
-	</form>
-</div>
+		</fieldset><div class="button-holder"><input alt="Search" class="submit" type="submit" value="Search"></div><input type="hidden" name="discoveryCustomSearch" value="true" /><input id="Hidden1" name="_ser" type="hidden" value="HO 334" /><input id="Hidden2" name="_ser" type="hidden" value="HO 409" /><input id="Hidden4" name="_ser" type="hidden" value="HO 45" /><input id="Hidden5" name="_ser" type="hidden" value="HO 144" /><input id="Hidden6" name="_ser" type="hidden" value="HO 382" /><input id="Hidden7" name="_hb" type="hidden" value="tna" /></form></div>
