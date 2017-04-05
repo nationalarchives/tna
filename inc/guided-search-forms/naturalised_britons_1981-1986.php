50b19b138da43e9d2acc90473dce031e323a7d15
grange85 <andy@grange85.co.uk>
2017-04-05 13:22:21 +0100

    add new tweaked guided searches for ARK ed

---
+++ inc/guided-search-forms/naturalised_britons_1981-1986.php
@@ -0,0 +1,20 @@
+<div class="research-guide-form">
+<form action="http://discovery.nationalarchives.gov.uk/results/r" method="get" name="searchform5532">
+<fieldset>
+<div class="field-row">
+<label for="Name">Name</label>
+<input id="Name" class="width-half" name="_q" />
+</div>
+</fieldset>
+<div class="button-holder"><input alt="Search" class="submit" type="submit" value="Search"></div>
+<input type="hidden" name="discoveryCustomSearch" value="true" />
+<input id="Hidden1" name="_ser" type="hidden" value="HO 334" />
+<input id="Hidden2" name="_ser" type="hidden" value="HO 409" />
+<input id="Hidden4" name="_ser" type="hidden" value="HO 45" />
+<input id="Hidden5" name="_ser" type="hidden" value="HO 144" />
+<input id="Hidden6" name="_ser" type="hidden" value="HO 382" />
+<input id="Hidden7" name="_hb" type="hidden" value="tna" />
+<input id="Hidden8" name="_sd" type="hidden" value="1981" />
+<input id="Hidden9" name="_ed" type="hidden" value="1986" />
+</form>
+</div>
