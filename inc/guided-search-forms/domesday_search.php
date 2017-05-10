600e14b29a4dda77b6944bec6622a2d468563514
grange85 <andy@grange85.co.uk>
2017-05-10 16:32:59 +0100

    domesday guided search

---
+++ inc/guided-search-forms/domesday_search.php
@@ -0,0 +1,17 @@
+<div class="research-guide-form">
+	<form action="http://discovery.nationalarchives.gov.uk/results/r" method="get" name="searchform5532">
+		<fieldset>
+			<div class="field-row">
+				<label for="Company name">Town or village</label>
+				<input id="domesday_search_text" class="width-half" name="_q" />
+			</div>
+		</fieldset>
+		<div class="button-holder">
+			<input alt="Search" class="submit" type="submit" value="Search">
+		</div>
+		<input id="Hidden1" name="_cr1" type="hidden" value="E 31">
+		<input id="Hidden2" name="_col" type="hidden" value="online">
+		<input id="Hidden3" name="_hb" type="hidden" value="tna">
+		<input id="Hidden4" name="_dt" type="hidden" value="D">
+	</form>
+</div>
