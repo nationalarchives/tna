f25c5953ff5a1ea406b257a8189ebd253dff7a9b
SDWilkes <simonwilkes@gmail.com>
2017-05-23 10:31:35 +0100

    Updating copy in banner

--- inc/home-promotion.php
+++ inc/home-promotion.php
@@ -1,5 +1,5 @@
 <div class="row" >
-    <div class="col starts-at-full ends-at-full box " style="background-image:url('<?php bloginfo('stylesheet_directory');?>/images/home/tna-banner-domesday-01.jpg'); background-repeat: repeat-x; background-position: left bottom;min-height:175px;margin-bottom:0px" title="Domesday Book at Lincoln Castle">
+    <div class="col starts-at-full ends-at-full box " style="background-image:url('<?php bloginfo('stylesheet_directory');?>/images/home/tna-banner-domesday-01.jpg'); background-repeat: repeat-x; background-position: left bottom;min-height:175px;margin-bottom:0px" title="Domesday Book goes to Lincoln Castle">
     </div>
     </div>
 
@@ -7,9 +7,9 @@
     <div class="col starts-at-full ends-at-full box ">
     <div class="float-left starts-at-full ends-at-two-thirds margin-none margin-bottom-small">
             <div class="pad-medium">
-            <h2 class="margin-none">Domesday Book at Lincoln Castle</h2>
+            <h2 class="margin-none">Domesday Book goes to Lincoln Castle</h2>
            27 May - 3 September<br>
-          Britain's earliest public record will make a once-in-a-lifetime trip to Lincoln Castle this summer.</div>
+          Britain's earliest public record will go on display at Lincoln Castle this summer.</div>
         </div>
         <div class="float-right starts-at-full ends-at-one-third margin-none margin-bottom-small">
             <div class="pad-large"><a href="http://www.nationalarchives.gov.uk/about/news/domesday-loan-to-lincoln-castle/" class="button float-right margin-top-medium" >Find out more</a>
