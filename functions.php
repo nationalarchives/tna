3706197c732bbf94a330ea1238ed674f8648a5a5
Chris Bishop <domingobishop@gmail.com>
2017-07-20 14:36:25 +0100

    removed add filter of og title

--- functions.php
+++ functions.php
@@ -793,8 +793,5 @@ add_filter( 'disable_wpseo_json_ld_search', '__return_true' );
 // Set path to mega menu HTML
 set_path_to_mega_menu(served_from_local_machine($_SERVER['SERVER_ADDR'], $_SERVER['REMOTE_ADDR']));
 
-//Yoast og title
-add_filter('wpseo_opengraph_title','new_ogtitle', 999);
-
 
 ?>
