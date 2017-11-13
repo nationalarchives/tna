7a36eea5369e84feaca67a5c4ee8a5c94f69487f
Annabel <annabel.baynes@gmail.com>
2017-11-13 11:29:12 +0000

    Added education-lesson-at-a-glance.php under /inc/

---
+++ inc/education-lesson-at-a-glance.php
@@ -0,0 +1,7 @@
+<?php
+
+
+$key_stage_strings = array ("ks1", "ks2", "ks3", "ks4", "ks5");
+$time_period_strings = array ("early-20th-century", "early-modern", "empire-and-industry", "interwar", "medieval", "postwar", "second-world-war", "victorians");
+
+$education_resource_meta = get_the_terms($post->ID, "education resource");
