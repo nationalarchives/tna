bd72372fe6cb26edc39ed5244fe11dafc808c8c0
Annabel <annabel.baynes@gmail.com>
2017-11-16 10:00:12 +0000

    Change formatting minorly (education-resource singular instead of plural)

--- inc/education-lesson-at-a-glance.php
+++ inc/education-lesson-at-a-glance.php
@@ -3,7 +3,7 @@
 function display_education_lesson_at_a_glance () {
 	$content = [];
 	foreach (get_section_keys_array() as $key => $section_key) {
-		if ($key === "education-resources") {
+		if ($key === "education-resource") {
 			foreach ( $section_key as $sub_section ) {
 				$content[] = format_content(retrieve_content( [ "key" => $key, "sub-section" => $sub_section ] ), $sub_section );
 			}
@@ -30,7 +30,7 @@ function get_html_box_lesson_at_a_glance ($content) {
 
 function get_section_keys_array () {
 	return [
-		"education-resources" => [ "key-stage", "time-period" ],
+		"education-resource" => [ "key-stage", "time-period" ],
 		"suggested-inquiry-questions",
 		"potential-activities",
 		"document-link"
@@ -39,9 +39,9 @@ function get_section_keys_array () {
 
 function retrieve_content ($section_keys) {
 	global $post;
-	if ($section_keys["key"] === "education-resources") {
+	if ($section_keys["key"] === "education-resource") {
 		$content_array = [];
-		foreach (get_the_terms( $post->ID, "education resource") as $education_resource_meta ) {
+		foreach (get_the_terms( $post->ID, remove_hyphen($section_keys["key"])) as $education_resource_meta ) {
 			if (slug_is_present_in_array($section_keys["sub-section"], $education_resource_meta)) {
 				$content_array[] =	$education_resource_meta;
 			}
