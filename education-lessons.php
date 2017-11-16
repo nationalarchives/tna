e3bd0b3d9290a9d47d567b35ca31e106facd1599
Annabel <annabel.baynes@gmail.com>
2017-11-16 14:36:48 +0000

    Use "lesson-at-a-glance" instead of "lesson_at_a_glance"

--- education-lessons.php
+++ education-lessons.php
@@ -57,7 +57,7 @@ get_header(); ?>
             </div>
             <!-- at a glance small screen start -->
                 <?php if (function_exists("display_education_side_box")) {
-                    echo display_education_side_box("lesson_at_a_glance");
+                    echo display_education_side_box("lesson-at-a-glance");
                 }  ?>
             <!--  at a glance small screen end  -->
             <div class="heading-holding-banner">
@@ -142,7 +142,7 @@ get_header(); ?>
         <div class="col starts-at-full ends-at-one-third clr box">
             <!-- at a glance large screen start -->
                 <?php if (function_exists("display_education_side_box")) {
-                    echo display_education_side_box("lesson_at_a_glance");
+                    echo display_education_side_box("lesson-at-a-glance");
                 }  ?>
             <!--  at a glance large screen end  -->
             <?php if (get_field('related-resources')){?>
