33995512a5156cbec8fe1ff079dc036f23d8e100
Annabel <annabel.baynes@gmail.com>
2017-11-29 10:57:13 +0000

    Change 'suggested learning objective' to 'suggested inquiry questions'

--- inc/custom-fields.php
+++ inc/custom-fields.php
@@ -1353,10 +1353,10 @@ add_action( 'add_meta_boxes', 'add_lesson_at_a_glance_metabox');
 
 function callback_lesson_at_a_glance_metabox ($post) {
 	wp_nonce_field("save_lesson_at_a_glance_metabox", "lesson_at_a_glance_nonce", true, true);
-	$suggested_learning_value = get_post_meta($post->ID, "suggested_learning_objective", true);
+	$suggested_inquiry_value = get_post_meta($post->ID, "suggested_inquiry_questions", true);
 	$potential_activities_value = get_post_meta($post->ID, "potential_activities", true);
-	echo "   <label for='suggested-learning-objective'>Suggested learning objective:</label>
-                 <input type='text' id='suggested-learning-objective' name='suggested-learning-objective' value='" .esc_attr($suggested_learning_value). "' class='widefat' />
+	echo "   <label for='suggested-inquiry-questions'>Suggested inquiry questions:</label>
+                 <input type='text' id='suggested-inquiry-questions' name='suggested-inquiry-questions' value='" .esc_attr($suggested_inquiry_value). "' class='widefat' />
                  <br><br>
                  <label for='potential-activities'>Potential activities:</label>
                  <input type='text' id='potential-activities' name='potential-activities' value='" .esc_attr($potential_activities_value). "' class='widefat'/>";
@@ -1366,9 +1366,9 @@ function save_lesson_at_a_glance_metabox ($post_id) {
 	if(!wp_verify_nonce( $_POST['lesson_at_a_glance_nonce'], 'save_lesson_at_a_glance_metabox' )){ return; }
 	if( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ){ return; }
 	if( !current_user_can( 'edit_post', $post_id ) ){ return; }
-	if(isset( $_POST["suggested-learning-objective"] )){
-		$suggested_learning_objective_data = sanitize_text_field( $_POST["suggested-learning-objective"]);
-		update_post_meta($post_id, "suggested_learning_objective", $suggested_learning_objective_data);
+	if(isset( $_POST["suggested-inquiry-questions"] )){
+		$suggested_inquiry_questions_data = sanitize_text_field( $_POST["suggested-inquiry-questions"]);
+		update_post_meta($post_id, "suggested_inquiry_questions", $suggested_inquiry_questions_data);
 	}
 	if(isset( $_POST["potential-activities"] )){
 		$potential_activities_data = sanitize_text_field( $_POST["potential-activities"]);
