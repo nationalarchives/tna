c465f55212a5f18c6f0d70c91639b583568c393b
grange85 <andy@grange85.co.uk>
2017-12-04 14:03:01 +0000

    Remove global and add breather class

--- inc/rss-feeds.php
+++ inc/rss-feeds.php
@@ -26,24 +26,23 @@ if (!function_exists('tna_rss')) :
 					if ( $n == 1 ) {
 						break;
 					}
-					global $html;
 					$enclosure  = $item->enclosure['url'];
 					$namespaces = $item->getNameSpaces( true );
 					$dc         = $item->children( $namespaces['dc'] );
 					$pubDate    = $item->pubDate;
 					$pubDate    = date( "l d M Y", strtotime( $pubDate ) );
 					if ( ! $image == 'no' ) {
-						$html .= '<a href="' . $url . '" title="' . $rssTitle . '"">';
+						$html = '<a href="' . $url . '" title="' . $rssTitle . '"">';
 						$html .= '<div class="image-container" style="background-image: url(' . $enclosure . ')">';
 						$html .= '<h2><span><span>' . $rssTitle . '</span></span></h2>';
 						$html .= '</div>';
 						$html .= '</a>';
 					}
-					$html .= '<h3 class="inline"><a href="' . $item->link . '">';
+					$html .= '<div class="breather"><h3 class="inline"><a href="' . $item->link . '">';
 					$html .= $item->title;
 					$html .= '</a></h3>';
 					$html .= '<div class="entry-meta">' . $dc->creator . ' | ' . $pubDate . '</div>';
-					$html .= '<p>' . $item->description . '</p>';
+					$html .= '<p>' . $item->description . '</p></div>';
 					$n ++;
 				endforeach;
 				set_transient( 'tna_rss_blog_transient' . $id, $html, HOUR_IN_SECONDS );
