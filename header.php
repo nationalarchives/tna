76f38d14b8bb60e9815556b26cf334b59128b5b6
Andy Aldridge <andrew.aldridge@nationalarchives.gsi.gov.uk>
2018-01-19 11:48:58 +0000

    update ga code to use https

--- header.php
+++ header.php
@@ -51,11 +51,10 @@ if (function_exists('the_field')){
 </head>
 
 <body>
-<!-- Google Tag Manager -->
-<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-T8DSWV"
-                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-
-<!-- End Google Tag Manager -->
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T8DSWV"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <?php
 if ( function_exists('tna_notification_banner') ){
 	tna_notification_banner();
--- inc/header-scripts.php
+++ inc/header-scripts.php
@@ -30,12 +30,12 @@
 <![endif]-->
 <!-- Google Tag Manager -->
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-T8DSWV');</script>
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-T8DSWV');</script>
 
-<!-- Google Tag Manager -->
+<!-- End Google Tag Manager -->
 <!-- iOS viewport fix -->
 <script src="/wp-content/themes/tna/scripts/ios-viewport-fix.js"></script>
 
