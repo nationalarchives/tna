da85081fc923e6297dc9ede44abef7280b089613
Mihai Diaconita <michaiel.webdesigner@gmail.com>
2019-01-07 13:31:55 +0000

    Update enqueued script version to today's date

--- functions.php
+++ functions.php
@@ -96,8 +96,9 @@ if (!function_exists('tna_dev_scripts')) :
         //wp_enqueue_script('tna-dev-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true);
 
         wp_enqueue_script('tna-dev-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true);
-	wp_enqueue_script('tna-guidance-feedback-component', 'https://nationalarchives.gov.uk/scripts/tna-components.js', array(), '20181228', true);
-        if (is_singular() && comments_open() && get_option('thread_comments')) {
+	wp_enqueue_script('tna-guidance-feedback-component', 'https://nationalarchives.gov.uk/scripts/tna-components.js', array(), '20190107', true);
+        
+	if (is_singular() && comments_open() && get_option('thread_comments')) {
             wp_enqueue_script('comment-reply');
         }
         if (is_page_template('level-1.php') && in_category('Education')) {
