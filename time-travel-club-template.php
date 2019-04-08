c996fbc83a3f73f60c6a7b5a35c26fe8eefd6717
NetDesignr <michaiel.webdesigner@gmail.com>
2019-04-08 15:30:30 +0100

    Add make_relative_path_from_url to the feature images

--- time-travel-club-template.php
+++ time-travel-club-template.php
@@ -9,7 +9,7 @@ get_header();
         <div class="row">
             <div class="col starts-at-full ends-at-two-thirds box clr">
                 <?php
-                $feat_image = wp_get_attachment_url(get_post_thumbnail_id($post->ID));
+                $feat_image = make_relative_path_from_url(wp_get_attachment_url(get_post_thumbnail_id($post->ID)));
                 ?>
                 <div class="image-container large position-relative pad-top-medium"
                      style="background-image: url('<?php echo $feat_image; ?>')">
