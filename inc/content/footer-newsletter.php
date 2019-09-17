cce0949a4f20fc4d21bd2f0396bb7ba32516ebac
AshTNA <ashleyaburnett@gmail.com>
2019-09-17 15:01:52 +0100

    Newsletter rebrand

--- inc/content/footer-newsletter.php
+++ inc/content/footer-newsletter.php
@@ -1,16 +1,15 @@
 <div class="newsletter-template">
     <div class="news-icon">
         <img src="https://www.nationalarchives.gov.uk/wp-content/themes/tna-base/img/tna-newsletter-icon.png"
-            alt="The National Archives Newsletter Icon" />
+             alt="The National Archives Newsletter Icon" />
     </div>
-
     <div class="news-content" id="newsletterAccessibility">
         <h2>Sign me up to the mailing list</h2>
         <p>Subscribe now for regular news, updates and priority booking for events</p>
     </div>
-
     <div class="news-form">
-        <a href="https://www.nationalarchives.gov.uk/contact-us/email-updates/" class="button">Sign up</a>
-        <div class="text-smallest margin-top-small"><a href="https://www.nationalarchives.gov.uk/legal/privacy-policy/"
-                title="About our privacy policy">About our privacy policy</a></div>
+        <a href="https://nationalarchives.gov.uk/contact-us/email-updates/" class="button">Sign up</a>
+        <div class="text-smallest margin-top-small"><a href="http://www.nationalarchives.gov.uk/legal/privacy-policy/"
+                                           title="About our privacy policy">About our privacy policy</a></div>
     </div>
+</div>
