03ba0050f91c2fd9f2e07772e14186c96a070441
domingobishop <domingobishop@gmail.com>
2019-09-18 12:23:27 +0100

    new newsletter markup

--- inc/content/footer-newsletter.php
+++ inc/content/footer-newsletter.php
@@ -1,13 +1,18 @@
-<div class="newsletter-template">
-    <div class="news-icon">
-        <img src="https://www.nationalarchives.gov.uk/wp-content/themes/tna-base/img/tna-newsletter-icon.png"
-             alt="The National Archives Newsletter Icon" />
-    </div>
-    <div class="news-content" id="newsletterAccessibility">
-        <h2>Sign me up to the mailing list</h2>
-        <p>Subscribe now for regular news, updates and priority booking for events.</p>
-    </div>
-    <div class="news-form">
-        <a href="https://nationalarchives.gov.uk/contact-us/email-updates/" class="button">Sign up</a>
+<div class="newsletter-tna-base">
+    <div class="container">
+        <div class="row newsletter-content">
+            <div class="col starts-at-full ends-at-one-quarter">
+                <div class="news-icon">
+                    <img src="https://www.nationalarchives.gov.uk/wp-content/themes/tna-base/img/tna-newsletter-icon.png"
+                         alt="The National Archives Newsletter Icon" />
+                </div>
+            </div>
+            <div class="col starts-at-full ends-at-two-thirds">
+                <div class="news-content" id="newsletterAccessibility">
+                    <h3>Sign me up to The National Archives' mailing list</h3>
+                    <p>Subscribe now for regular news, updates and priority booking for events.<br><a href="https://www.nationalarchives.gov.uk/contact-us/email-updates/" class="button">Sign up</a></p>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
