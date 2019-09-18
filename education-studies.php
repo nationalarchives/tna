2b9edfaf7e0eeb51d227324d8f3025856c4ddaa9
JamesWChan <jwchan1993@gmail.com>
2019-09-18 13:40:48 +0100

    education-studies.php fix padding

--- education-studies.php
+++ education-studies.php
@@ -13,27 +13,27 @@ get_header(); ?>
 </style>
 <?php endif;?>
 
-<div id="page_wrap" class="container" role="main"> 
+<div id="page_wrap" class="container" role="main">
   <!-- Breadcrumbs -->
   <?php include 'breadcrumb.php'; ?>
-  <?php 
-	
-	if ( have_posts() ) : 
-	
-	
+  <?php
+
+	if ( have_posts() ) :
+
+
 	?>
   <?php while ( have_posts() ) : the_post(); ?>
-  
-  <!-- content goes here --> 
+
+  <!-- content goes here -->
   <a name="top"></a>
   <div class="row">
     <div class="col starts-at-full ends-at-two-thirds box clr">
       <div id="content-banner" class="image-container small position-relative pad-top-medium">
-        <h1 class="margin-none"><span><span>
+        <h1 class="margin-none"><span><span class="education-child-header">
           <?php the_title(); ?>
           </span></span></h1>
       </div>
-      <div class="breather clr">
+      <div class="breather clr no-left-right-padding">
         <?php the_content(); ?>
         <?php if(get_field('button-text')): ?>
         <a href="<?php the_field('button-url'); ?>" class="button float-right">
@@ -42,7 +42,7 @@ get_header(); ?>
         <?php endif; ?>
       </div>
     </div>    <?php if (get_field('related-resources')){?>
-    <div class="col starts-at-full ends-at-one-third clr box"> 
+    <div class="col starts-at-full ends-at-one-third clr box">
       <!-- related resources -->
       <div class="heading-holding-banner">
         <h2> <span> <span>Related studies</span> </span> </h2>
@@ -50,11 +50,11 @@ get_header(); ?>
       <div class="breather">
         <?php
 		$post_objects = get_field('related-studies');
- 
+
 		if( $post_objects ): ?>
         <div class="pictorial-list grid-within-grid-one-item">
           <?php foreach( $post_objects as $post): // variable must be called $post (IMPORTANT) ?>
-          <?php setup_postdata($post); 
+          <?php setup_postdata($post);
           $image_url = wp_get_attachment_image_src( get_post_thumbnail_id());?>
           <div class="resource-block margin-bottom-small">
           <a href="<?php the_permalink(); ?>"><div style="background-image: url(<?php echo $image_url[0] ?>)"></div>
@@ -65,11 +65,11 @@ get_header(); ?>
             <p>
               <?php
              if (get_field('subheading')){
-            
+
              echo ($resourcequestion);
              }else{
-               
-            if (has_excerpt()){the_excerpt();}   
+
+            if (has_excerpt()){the_excerpt();}
              }
              ?></p></div>
           <?php endforeach; ?>
