f5b821ab23979ba452db39641a131aa147320302
domingobishop <domingobishop@gmail.com>
2019-09-19 14:11:39 +0100

    new footer styles

--- footer.php
+++ footer.php
@@ -154,20 +154,11 @@ endif;
                             <img src="https://www.nationalarchives.gov.uk/wp-content/themes/tna-base/img/social/rss.png"
                                 alt="RSS logo"></a>
                     </div>
+				</div>
+				<div class="footer-left">
                 </div>
-                <div class="footer-left">
-                    <h4>Featured Flickr image</h4>
-                    <p id="flickr-caption" class="caption">Patented textile pattern by Christopher Dresser. <a
-                            target="_blank" title="External website - opens in a new window"
-                            href="http://www.flickr.com/photos/nationalarchives/3208858799/in/set-72157608416657334/">EXT
-                            9/104</a></p>
-                    <p id="flickr-image"> <a target="_blank" title="External website - opens in a new window"
-                            href="http://www.flickr.com/photos/nationalarchives/3208858799/in/set-72157608416657334/">
-                            <img src="https://www.nationalarchives.gov.uk/images/global/featured-flickr-ext-9-104.jpg"
-                                alt="Patented textile pattern by Christopher Dresser."></a> </p>
-                </div>
-            </div>
-        </div>
+			</div>
+		</div>
 
         <!-- end row -->
         <div class="row footer-base">
