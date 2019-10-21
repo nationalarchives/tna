fbc6192928e794ba4d6f89a78a5c8abab04d2e64
NetDesignr <michaiel.webdesigner@gmail.com>
2019-10-21 13:07:57 +0100

    Update newsletter

--- inc/content/footer-newsletter.php
+++ inc/content/footer-newsletter.php
@@ -1,6 +1,7 @@
 <div class="newsletter-template">
     <div class="news-icon">
-        <img src="https://www.nationalarchives.gov.uk/wp-content/themes/tna-base/img/tna-newsletter-icon.png" alt="The National Archives Newsletter Icon"/>
+        <img src="https://www.nationalarchives.gov.uk/wp-content/themes/tna-base/img/tna-newsletter-icon.png"
+            alt="The National Archives Newsletter Icon" />
     </div>
 
     <div class="news-content" id="newsletterAccessibility">
@@ -9,21 +10,7 @@
     </div>
 
     <div class="news-form">
-        <form name="signup" id="signup" action="https://app.wiredplus.com/form-signup" method="post" role="form">
-
-			<input type="hidden" name="signup" value="NDQ1">
-			<input type="hidden" name="return_url" value="https://www.nationalarchives.gov.uk/notifications/newsletter-thank-you/">
-			<input type="hidden" name="confirmation_subject" value="">
-			<input type="hidden" name="confirmation" value="">
-			<input type="hidden" name="list" value="3">
-			<input type="hidden" name="source" value="web">
-			<input type="hidden" name="opt_in" value="1">
-
-            <label for="Email">Send me The National Archives’ newsletter</label>
-            <input type="email" name="email" id="email" required="required"
-                   placeholder="Enter your email address" aria-labelledby="newsletterAccessibility">
-            <input id="newsletterSignUp" type="submit" name="Submit" value="Submit" class="margin-left-medium">
-        </form>
+        <a href="https://www.nationalarchives.gov.uk/contact-us/email-updates/" class="button">Sign up</a>
         <div class="text-smallest margin-top-small"><a href="https://www.nationalarchives.gov.uk/legal/privacy-policy/"
-                                           title="About our privacy policy">About our privacy policy</a></div>
+                title="About our privacy policy">About our privacy policy</a></div>
     </div>
