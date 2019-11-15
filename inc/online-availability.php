96430f19d142b4a343657378a962c38b67f3ecdd
AshTNA <ashleyaburnett@gmail.com>
2019-11-15 13:37:41 +0000

    Fix accessibility flow of tabpanel

--- inc/online-availability.php
+++ inc/online-availability.php
@@ -8,9 +8,9 @@
           $copies_available = 'We can either <a href="/help-with-your-research/record-copying/">copy our records</a> onto paper or deliver them to you digitally';
 
           // On state variables
-          $highlight_none = '<ul><li class="none-small">None</li><li>Some</li><li>All</li></ul>';
-          $highlight_some = '<ul><li>None</li><li class="some-small">Some</li><li>All</li></ul>';
-          $highlight_all = '<ul><li>None</li><li>Some</li><li class="all-small">All</li></ul>';
+          $highlight_none = '<ul role="presentation"><li class="none-small">None</li><li aria-hidden="true">Some</li><li aria-hidden="true">All</li></ul>';
+          $highlight_some = '<ul role="presentation"><li aria-hidden="true">None</li><li class="some-small">Some</li><li aria-hidden="true">All</li></ul>';
+          $highlight_all = '<ul role="presentation"><li aria-hidden="true">None</li><li aria-hidden="true">Some</li><li class="all-small">All</li></ul>';
 
           // Icon style variables
           $icon_none = 'none';
@@ -22,49 +22,54 @@
 //          $format = '<div class="indicator" role="tabpanel" aria-label="How can I view the records review in this guide?" id="delivery-options-tablist"> <div class="item"> <button <button role="tab" aria-selected="true" aria-controls="delivery-options-tablist" id="view-online" class="toggle-heading no-print %s">View online</button> <div class="indicator-description clr" aria-labelledby="view-online"> <div class="breather"> <p>How many are online?</p>%s</div></div></div><div class="item no-print"> <h3 class="toggle-heading %s" id="order-copies">Order copies</h3> <div class="indicator-description clr" aria-labelledby="order-copies"> <div class="breather"> <p>%s</p></div></div></div><div class="item no-print"> <h3 class="toggle-heading" id="visit-us">Visit us in Kew</h3> <div class="indicator-description clr" aria-labelledby="visit-us"> <div class="breather"> <p><a href="/about/visit-us/">Visit us in Kew</a> to see original documents or view online records for free</p></div></div></div><div class="item no-print"> <h3 class="toggle-heading" id="pay-research">Pay for research</h3> <div class="indicator-description clr" aria-labelledby="pay-research"> <div class="breather"> <p>Consider <a href="/help-with-your-research/paying-for-research">paying for research</a></p></div></div></div></div>';
 
           $format = <<<HTML
-            <div class="indicator" role="tablist" aria-label="How can I view the records review in this guide?" id="delivery-options-tablist">
+<div class="indicator" role="tablist" aria-label="How can I view the records review in this guide?" id="delivery-options-tablist">
 
-
-        <div class="item">
-            <button role="tab" aria-selected="true" aria-controls="delivery-options-tablist" id="view-online" class="toggle-heading no-print %s">
-                View Online
-            </button>
-            <div class="indicator-description clr" aria-labelledby="view-online">
-                <div class="breather"><p>How many are online?</p> %s</div>
-            </div>
+    <div class="item">
+        <button role="tab" aria-selected="true" aria-controls="delivery-options-tablist" id="view-online" class="toggle-heading no-print %s">
+            View Online
+        </button>
+        <div class="indicator-description clr" aria-labelledby="view-online" role="tabpanel" tabindex="0">
+            <div class="breather">
+                <p>How many are online?</p> %s</div>
         </div>
-
-        <div class="item no-print">
-            <button role="tab" aria-selected="false" aria-controls="delivery-options-tablist" class="toggle-heading %s" id="order-copies">
-                Order copies
-            </button>
-            <div class="indicator-description clr" aria-labelledby="order-copies">
-               <div class="breather"><p>%s</p></div>
-            </div>
-        </div>
-
-        <div class="item no-print">
-            <button role="tab" aria-selected="false" aria-controls="delivery-options-tablist" class="toggle-heading" id="visit-us">
-                Visit us in Kew
-            </button>
-            <div class="indicator-description clr" aria-labelledby="visit-us">
-                <div class="breather"><p><a href="/about/visit-us/">Visit us in Kew</a> to see original documents or view
-                    online records for free</p></div>
-            </div>
-        </div>
-
-        <div class="item no-print">
-            <button role="tab" aria-selected="false" aria-controls="delivery-options-tablist" id="pay-research" class="toggle-heading">
-                Pay for research
-            </button>
-            
-            <div class="indicator-description clr" aria-labelledby="pay-research">
-                <div class="breather"><p>Consider <a href="/help-with-your-research/paying-for-research">paying for
-                    research</a></p></div>
-            </div>
-        </div>
-
     </div>
+
+    <div class="item no-print">
+        <button role="tab" aria-selected="false" aria-controls="delivery-options-tablist" class="toggle-heading %s" id="order-copies">
+            Order copies
+        </button>
+        <div class="indicator-description clr" aria-labelledby="order-copies" role="tabpanel" tabindex="0">
+            <div class="breather">
+                <p>%s</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="item no-print">
+        <button role="tab" aria-selected="false" aria-controls="delivery-options-tablist" class="toggle-heading" id="visit-us">
+            Visit us in Kew
+        </button>
+        <div class="indicator-description clr" aria-labelledby="visit-us" role="tabpanel" tabindex="0">
+            <div class="breather">
+                <p><a href="/about/visit-us/">Visit us in Kew</a> to see original documents or view online records for free</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="item no-print">
+        <button role="tab" aria-selected="false" aria-controls="delivery-options-tablist" id="pay-research" class="toggle-heading">
+            Pay for research
+        </button>
+
+        <div class="indicator-description clr" aria-labelledby="pay-research" role="tabpanel"  tabindex="0">
+            <div class="breather">
+                <p>Consider <a href="/help-with-your-research/paying-for-research">paying for
+                    research</a></p>
+            </div>
+        </div>
+    </div>
+
+</div>
 HTML;
 
           // If all are online...
