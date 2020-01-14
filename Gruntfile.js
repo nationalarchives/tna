8c29eb4e2d7324d277c77962b2057fb015c5bb4d
AshTNA <ashleyaburnett@gmail.com>
2020-01-14 11:22:09 +0000

    Fix Gruntfile.js conflict

--- Gruntfile.js
+++ Gruntfile.js
@@ -41,66 +41,13 @@ module.exports = function(grunt) {
 		}
 	});
 
-<<<<<<< HEAD
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-browser-sync');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
-=======
-    // Project configuration.
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        sass: {
-            options: {
-                sourcemap: false
-            },
-            dist: {
-                files: {
-                    'css/base-sass.css': 'css/sass/base-sass.scss'
-                }
-            }
-        },
-        cssmin: {
-            options: {
-                sourceMap: true
-            },
-            target: {
-                files: {
-                    'css/base-sass.css.min': ['css/base-sass.css']
-                }
-            }
-        },
-        watch: {
-            css: {
-                files: 'css/sass/*.scss',
-                tasks: ['sass', 'cssmin']
-            }
-        },
-        browserSync: {
-            dev: {
-                bsFiles: {
-                    src: [
-                        'css/*.css'
-                    ]
-                },
-                options: {
-                    watchTask: true,
-                    proxy: 'tna-website-dev:8888'
-                }
-            }
-        }
-    });
->>>>>>> npm audit fix & grunt fix
 
 	// Default task(s).
 	grunt.registerTask('default', ['sass', 'cssmin', 'watch']);
 	grunt.registerTask('bSync', ['browserSync', 'watch']);
 
-<<<<<<< HEAD
-=======
-    // Default task(s).
-    grunt.registerTask('default', ['sass', 'cssmin', 'watch']);
-    grunt.registerTask('bSync', ['browserSync', 'watch']);
-
->>>>>>> npm audit fix & grunt fix
 };
