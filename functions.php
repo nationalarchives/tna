ae3ecdbf87e52d53e4a89e4c64c8024d5d879599
Ash <8880610+AshTNA@users.noreply.github.com>
2020-06-09 11:16:08 +0100

    Update tna-components version

--- functions.php
+++ functions.php
@@ -96,7 +96,7 @@ if (!function_exists('tna_dev_scripts')) :
         //wp_enqueue_script('tna-dev-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true);
 
         wp_enqueue_script('tna-dev-skip-link-focus-fix', str_replace(home_url(), '', get_template_directory_uri()) . '/js/skip-link-focus-fix.js', array(), '20190329', true);
-	wp_enqueue_script('tna-components', 'https://cdn.nationalarchives.gov.uk/react-components/dist/website-1.1.2.js', array(), '20200608', true);
+	wp_enqueue_script('tna-components', 'https://cdn.nationalarchives.gov.uk/react-components/dist/website-1.1.3.js', array(), '20200608', true);
 
 	if (is_singular() && comments_open() && get_option('thread_comments')) {
             wp_enqueue_script('comment-reply');
