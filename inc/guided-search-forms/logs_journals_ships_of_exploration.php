ea75fa09ed3dec2997932d698b52eeacbe0d619b
AshTNA <ashleyaburnett@gmail.com>
2021-02-16 16:06:42 +0000

    Fix logs_journals_ships_of_exploration form

--- inc/guided-search-forms/logs_journals_ships_of_exploration.php
+++ inc/guided-search-forms/logs_journals_ships_of_exploration.php
@@ -1,11 +1,9 @@
 <div class="research-guide-form">
-	<form action="https://discovery.nationalarchives.gov.uk/results/r" method="get" name="searchform5532">
-		<fieldset>
+	<form action="https://discovery.nationalarchives.gov.uk/results/r" method="get" name="searchform5532" role="search" aria-label="Search the records">
 			<div class="field-row">
-				<label for="Keyword search">Keyword search</label>
+				<label for="keywordsearch">Keyword search</label>
 				<input id="keywordsearch" class="width-half" name="_q" />
 			</div>
-		</fieldset>
 		<div class="button-holder">
 			<input alt="Search" class="submit" type="submit" value="Search">
 		</div>
