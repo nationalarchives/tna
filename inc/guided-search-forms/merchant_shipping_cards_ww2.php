a9224cc8c928f455dad9e539c105a23d7ee958aa
AshTNA <ashleyaburnett@gmail.com>
2021-02-18 13:44:07 +0000

    Fix 'Merchant shipping movement cards 1939-1945' form

--- inc/guided-search-forms/merchant_shipping_cards_ww2.php
+++ inc/guided-search-forms/merchant_shipping_cards_ww2.php
@@ -1,11 +1,9 @@
 <div class="research-guide-form">
-	<form action="https://discovery.nationalarchives.gov.uk/results/r" method="get" name="searchform5532">
-		<fieldset>
+	<form action="https://discovery.nationalarchives.gov.uk/results/r" method="get" name="searchform5532" role="search" aria-label="Search the records">
 			<div class="field-row">
-				<label for="Ship name">Ship name</label>
+				<label for="Shipname">Ship name</label>
 				<input id="Shipname" class="width-half" name="_q" />
 			</div>
-		</fieldset>
 		<div class="button-holder">
 			<input alt="Search" class="submit" type="submit" value="Search">
 		</div>
