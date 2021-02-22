886bac74ead01ad8a218cb4b65ffae201e45e5c5
AshTNA <ashleyaburnett@gmail.com>
2021-02-22 14:09:28 +0000

    Fix 'Officers in the Merchant Navy' form

--- inc/guided-search-forms/officer_merchant_navy.php
+++ inc/guided-search-forms/officer_merchant_navy.php
@@ -1,11 +1,9 @@
 <div class="research-guide-form">
-	<form action="https://discovery.nationalarchives.gov.uk/results/r" method="get" name="searchform5532">
-		<fieldset>
-			<div class="field-row">
-				<label for="Name of person or ship">Name of person or ship</label>
-				<input id="Nameofpersonorship" class="width-half" name="_q" />
-			</div>
-		</fieldset>
+	<form action="https://discovery.nationalarchives.gov.uk/results/r" method="get" name="searchform5532" role="search" aria-label="Search the records">
+		<div class="field-row">
+			<label for="Nameofpersonorship">Name of person or ship</label>
+			<input id="Nameofpersonorship" class="width-half" name="_q" />
+		</div>
 		<div class="button-holder">
 			<input alt="Search" class="submit" type="submit" value="Search">
 		</div>
