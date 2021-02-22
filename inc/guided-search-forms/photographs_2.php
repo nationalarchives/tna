c14456845559e269d615ea4e5459a27df4eed4ab
AshTNA <ashleyaburnett@gmail.com>
2021-02-22 14:48:03 +0000

    Fix 'photographs' forms
    I can't see these forms being used, so they haven't been tested.

--- inc/guided-search-forms/photographs_2.php
+++ inc/guided-search-forms/photographs_2.php
@@ -1,11 +1,9 @@
 <div class="research-guide-form">
-	<form action="https://discovery.nationalarchives.gov.uk/results/r" method="get" name="searchform5532">
-		<fieldset>
-			<div class="field-row">
-				<label for="Country">Country</label>
-				<input id="Country" class="width-half" name="_q" />
-			</div>
-		</fieldset>
+	<form action="https://discovery.nationalarchives.gov.uk/results/r" method="get" name="searchform5532" role="search" aria-label="Search the records">
+		<div class="field-row">
+			<label for="Country">Country</label>
+			<input id="Country" class="width-half" name="_q" />
+		</div>
 		<div class="button-holder">
 			<input alt="Search" class="submit" type="submit" value="Search">
 		</div>
