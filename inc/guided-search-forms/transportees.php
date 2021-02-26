6991f7b1d4de8664c2f4714b3106fe823fd32352
AshTNA <ashleyaburnett@gmail.com>
2021-02-26 15:27:11 +0000

    Fix 'transportees' form
    I can't find this form being used locally, so it hasn't been tested

--- inc/guided-search-forms/transportees.php
+++ inc/guided-search-forms/transportees.php
@@ -1,11 +1,9 @@
 <div class="research-guide-form">
-	<form action="https://discovery.nationalarchives.gov.uk/results/r" method="get" name="searchform5532">
-		<fieldset>
+	<form action="https://discovery.nationalarchives.gov.uk/results/r" method="get" name="searchform5532" role="search" aria-label="Search the records">
 			<div class="field-row">
-				<label for="Name of ship or convict">Name of ship or convict</label>
+				<label for="Nameofshiporconvict">Name of ship or convict</label>
 				<input id="Nameofshiporconvict" class="width-half" name="_q" />
 			</div>
-		</fieldset>
 		<div class="button-holder">
 			<input alt="Search" class="submit" type="submit" value="Search">
 		</div>
