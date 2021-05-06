81c5786f6c73d64e2de23b443491c6b18abd0119
AshTNA <8880610+AshTNA@users.noreply.github.com>
2021-05-06 11:21:14 +0100

    Remove unnecessary fieldset

--- inc/guided-search-forms/colonies_and_dependencies_2.php
+++ inc/guided-search-forms/colonies_and_dependencies_2.php
@@ -1,11 +1,9 @@
 <div class="research-guide-form">
 	<form action="https://discovery.nationalarchives.gov.uk/results/r" method="get" name="searchform5532" role="search" aria-label="A guided search of the records">
-		<fieldset>
 			<div class="field-row">
 				<label for="Country">Country</label>
 				<input id="Country" class="width-half" name="_q" />
 			</div>
-		</fieldset>
 		<div class="button-holder">
 			<input alt="Search" class="submit" type="submit" value="Search">
 		</div>
