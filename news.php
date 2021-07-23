4820ba8a889f6e482e318476025abd280d9631c1
Chris Bishop <domingobishop@users.noreply.github.com>
2021-07-23 11:13:43 +0100

    update news pagination to current wp pagination format

--- news.php
+++ news.php
@@ -198,7 +198,7 @@ if ( $strtype == "" ) {
 							$big = 999999999; // need an unlikely integer
 							echo paginate_links( array(
 								//'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
-								'format'  => '?paged=%#%',
+								'format'  => '/page/%#%',
 								'current' => max( 1, get_query_var( 'paged' ) ),
 								'total'   => $wp_query->max_num_pages
 							) );
