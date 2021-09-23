c1f317b2b70205e9be5bbef8d7e2305011e27cd8
AshTNA <ashleyaburnett@gmail.com>
2021-09-23 11:57:08 +0100

    Add https to global search fallback links

--- inc/global-search.php
+++ inc/global-search.php
@@ -2,8 +2,8 @@
     <div class="global-search-no-js-fallback mobile">
     <nav>
         <ul>
-            <li><a href="http://www.nationalarchives.gov.uk/search/">Search our website</a></li>
-            <li><a href="http://discovery.nationalarchives.gov.uk">Search Discovery, our catalogue</a></li>
+            <li><a href="https://www.nationalarchives.gov.uk/search/">Search our website</a></li>
+            <li><a href="https://discovery.nationalarchives.gov.uk">Search Discovery, our catalogue</a></li>
         </ul>
     </nav>
     </div>
@@ -12,8 +12,8 @@
     <div class="global-search-no-js-fallback desktop">
         <nav>
             <ul>
-                <li><a href="http://www.nationalarchives.gov.uk/search/">Search our website</a></li>
-                <li><a href="http://discovery.nationalarchives.gov.uk">Search Discovery, our catalogue</a></li>
+                <li><a href="https://www.nationalarchives.gov.uk/search/">Search our website</a></li>
+                <li><a href="https://discovery.nationalarchives.gov.uk">Search Discovery, our catalogue</a></li>
             </ul>
         </nav>
     </div>
