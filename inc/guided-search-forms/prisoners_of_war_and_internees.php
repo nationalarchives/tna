98e2959ee6a6b59e2cf468190c29e0db114849c8
Andy Aldridge <andy@grange85.co.uk>
2023-09-07 12:35:29 +0100

    add pow and internees search form first draft

---
+++ inc/guided-search-forms/prisoners_of_war_and_internees.php
@@ -0,0 +1,20 @@
+<div class="research-guide-form">
+    <form action="https://discovery.nationalarchives.gov.uk/results/r" method="get" name="searchform5532" role="search" aria-label="A guided search of the records">
+
+            <div class="field-row"><label class="label-with-input">Name
+        <input class="width-half" name="_q" />
+        </label></div>
+
+        <div class="button-holder"><input alt="Search" class="submit" type="submit" value="Search"></div>
+
+		<input type="hidden" name="discoveryCustomSearch" value="true" />
+		<input name="_ser" type="hidden" value="WO 416" />
+		<input name="_ser" type="hidden" value="WO 208" />
+		<input name="_ser" type="hidden" value="BT 373" />
+		<input name="_ser" type="hidden" value="AIR 81" />
+		<input name="_ser" type="hidden" value="ADM 358" />
+		<input name="_ser" type="hidden" value="AIR 40" />
+		<input name="_ser" type="hidden" value="CO 170" />
+		<input name="_hb" type="hidden" value="tna" />
+    </form>
+</div>
