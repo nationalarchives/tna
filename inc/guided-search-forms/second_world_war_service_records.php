c664537f280259c7d3dbed9881f276464a265b81
Andy Aldridge <andy@grange85.co.uk>
2024-02-22 16:24:50 +0000

    new guided search

---
+++ inc/guided-search-forms/second_world_war_service_records.php
@@ -0,0 +1,34 @@
+<div class="research-guide-form">
+	<form action="https://discovery.nationalarchives.gov.uk/results/r" id="dolSearch" method="get" name="searchform5532" role="search" aria-label="A guided search of the records">
+    	<fieldset>
+		<legend>Details of individual:</legend>
+			<div class="field-row">
+				<label for="firstname">Initials</label>
+				<input id="firstname" class="inlineSearch" name="_fn">
+			</div>
+			
+			<div class="field-row">
+				<label for="lastname">Last name</label>
+				<input id="lastname" class="inlineSearch" name="_ln">
+			</div>
+			
+			<div class="field-row">
+				<label for="servicenumber">Service number</label>
+				<input id="servicenumber" class="inlineSearch" name="_no">
+			</div>
+		</fieldset>
+		<div class="button-holder">
+			<input class="submit" type="submit" value="Search">
+		</div>
+		<input name="discoveryCustomSearch" value="true" type="hidden">
+		<input id="Hidden1" name="_cr" value="WO 420" type="hidden">
+		<input id="Hidden1" name="_cr" value="WO 421" type="hidden">
+		<input id="Hidden1" name="_cr" value="WO 422" type="hidden">
+		<input id="Hidden1" name="_cr" value="WO 423" type="hidden">
+		<input id="Hidden1" name="_cr" value="WO 427" type="hidden">
+		<input id="Hidden2" name="_sd" value="1939" type="hidden">
+		<input id="Hidden3" name="_ed" value="1945" type="hidden">
+		<input id="Hidden4" name="_hb" value="tna" type="hidden">
+	</form>
+</div>
+
