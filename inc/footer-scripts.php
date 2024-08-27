6d13220ff9524bb2a0dd19b1caf4abbe9ba17f61
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2024-08-27 14:11:58 +0100

    Disable analytics

--- inc/footer-scripts.php
+++ inc/footer-scripts.php
@@ -4,9 +4,9 @@
 	if(window.TNAFrontend && window.TNAFrontend.initAll) {
 		window.TNAFrontend.initAll()
 	}
-	if(window.TNAFrontendAnalytics && window.TNAFrontendAnalytics.GA4) {
-		new TNAFrontendAnalytics.GA4({ addTrackingCode: false })
-	}
+	// if(window.TNAFrontendAnalytics && window.TNAFrontendAnalytics.GA4) {
+	// 	new TNAFrontendAnalytics.GA4({ addTrackingCode: false })
+	// }
 </script>
 
 <?php
