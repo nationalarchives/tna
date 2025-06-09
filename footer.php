97462aee53bc2505634c5fefb2f20ee6254de0ed
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-06-09 09:42:37 +0100

    Correct footer link text from "Blog" to "Blogs"

--- footer.php
+++ footer.php
@@ -101,7 +101,7 @@
                         </li>
                         <li class="tna-footer__navigation-block-item">
                             <a href="https://www.nationalarchives.gov.uk/blogs/" class="tna-footer__navigation-block-item-link">
-                                Blog
+                                Blogs
                             </a>
                         </li>
                         <li class="tna-footer__navigation-block-item">
